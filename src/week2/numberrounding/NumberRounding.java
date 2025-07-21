f27f820589a25d8c644a6d24179b88738a3af854
Uğur Taşdelen <ugurtasdelen1234@gmail.com>
2025-07-21 19:08:14 +0300

    Number rounding added

--- src/week2/numberrounding/NumberRounding.java
+++ src/week2/numberrounding/NumberRounding.java
@@ -1,4 +1,18 @@
 package week2.numberrounding;
 
+import java.util.Scanner;
+
 public class NumberRounding {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bir ondalıklı sayı giriniz: ");
+        double input = scanner.nextDouble();
+
+        System.out.println("Aşağı Yuvarlama: "+ Math.floor(input));
+        System.out.println("Yukarı Yuvarlama: "+ Math.ceil(input));
+        System.out.println("En Yakın Tam Sayı: "+ Math.round(input));
+
+    }
 }
---
+++ src/week2/numberrounding/README.md
@@ -0,0 +1,12 @@
+# Number Rounding Algorithm
+***
+## Rounds the entered decimal number up, down, and to the nearest integer.
+### For this process we have 3 methods from Math Class: 
+* Math.floor
+* Math.ceil
+* Math.round
+```java
+        System.out.println("Aşağı Yuvarlama: "+ Math.floor(input));
+        System.out.println("Yukarı Yuvarlama: "+ Math.ceil(input));
+        System.out.println("En Yakın Tam Sayı: "+ Math.round(input));
+```
