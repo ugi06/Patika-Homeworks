8bf073492fba8e6f1bca30c31576433810642693
Uğur Taşdelen <ugurtasdelen1234@gmail.com>
2025-08-13 13:24:05 +0300

    printArray method added.

--- src/week3/printarraymethod/Main.java
+++ src/week3/printarraymethod/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Yazdırılacak 4 wrapper class arrayleri
+        // Yazdırılacak wrapper class arrayleri
 
         String[] elements = {"Sigarayı","acilen","bırakmalısın", "." , "Eğer","bırakmaszsan","................"};
 
