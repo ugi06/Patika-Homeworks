d53f226664b77f5e43befc9147443c785678ab9d
Uğur Taşdelen <ugurtasdelen1234@gmail.com>
2025-08-19 12:20:59 +0300

    Inner Class added.

--- src/week5/ınnerclasses/Employee.java
+++ src/week5/ınnerclasses/Employee.java
@@ -50,7 +50,7 @@ public class Employee {
     public static void main(String[] args) {
 
         Employee employee1 = new Employee("Uğur","Taşdelen","Backend Developer","ugurtasdelen1234@gmail.com","5346904018");
-        System.out.println(employee1);
+        System.out.println(employee1.contactInfo);
 
 
     }
